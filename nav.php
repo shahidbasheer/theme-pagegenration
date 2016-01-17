461ecc7c32f1eaf4d23bf3ae79965767905a10a0
Shahid Bashir <shahid.chodhry@gmail.com>
2016-01-18 00:15:11 +0500

    update nav fix

--- nav.php
+++ nav.php
@@ -13,11 +13,11 @@
                 </div>
                 <div class="collapse navbar-collapse js-navbar-collapse">
                     <ul class="nav navbar-nav">
-                            <li><a href="index.php"><strong>What is Hair Loss</strong></a></li>
-                            <li><a href="before-and-after.php"><strong>Hair Loss for Men</strong></a></li>
-                            <li><a href="testimonials.php"><strong>Hair Loss for Women</strong></a></li>
-                            <li><a href="registration.php"><strong>Visit Dr GVG Asthetic Clinic</strong></a></li>
-                            <li><a href="registration.php"><strong>Contact Us</strong></a></li>
+                            <li><a href="#"><strong>What is Hair Loss</strong></a></li>
+                            <li><a href="#"><strong>Hair Loss for Men</strong></a></li>
+                            <li><a href="#"><strong>Hair Loss for Women</strong></a></li>
+                            <li><a href="#"><strong>Visit Dr GVG Asthetic Clinic</strong></a></li>
+                            <li><a href="#"><strong>Contact Us</strong></a></li>
                         </ul>
                     </li>
                     
