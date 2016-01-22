21a0d7a02f3670ba0131f2fb09a98b8238550aa8
Shahid Bashir <shahid.chodhry@gmail.com>
2016-01-22 20:05:10 +0500

    wordpressify

--- nav.php
+++ nav.php
@@ -13,10 +13,10 @@
                 </div>
                 <div class="collapse navbar-collapse js-navbar-collapse">
                     <ul class="nav navbar-nav">
-                            <li><a href="#"><strong>What is Hair Loss</strong></a></li>
+                            <li><a  target="_blank" href="http://www.blog.drgvg.co.in/identifying-what-stage-is-your-hair-loss/"><strong>What is Hair Loss</strong></a></li>
                             <li><a href="#"><strong>Hair Loss for Men</strong></a></li>
                             <li><a href="#"><strong>Hair Loss for Women</strong></a></li>
-                            <li><a href="#"><strong>Visit Dr GVG Asthetic Clinic</strong></a></li>
+                            <li><a  target="_blank" href="http://drgvg.co.in/"><strong>Visit Dr GVG Asthetic Clinic</strong></a></li>
                             <li><a href="#"><strong>Contact Us</strong></a></li>
                         </ul>
                     </li>
