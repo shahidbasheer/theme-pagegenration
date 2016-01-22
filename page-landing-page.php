d852a6d0b080460d1791185d5476289da46bc851
Shahid Bashir <shahid.chodhry@gmail.com>
2016-01-22 13:41:39 +0500

    wordpressify

--- page-landing-page.php
+++ page-landing-page.php
@@ -23,16 +23,17 @@ Template Name: landing-page
                 <div id="landing-page">
                     
                 
-               <?php 
-               if ( have_posts() ) {
-                    while ( have_posts() ) {
-                        the_post(); 
-                        //
-                        the_title();
-                        //
-                    } // end while
-               } // end if
-               ?>
+               <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                    
+                    <?php the_content(); ?>
+              
+
+              <?php endwhile;?>
+            
+                <?php else : ?>
+                <p> no content </p>
+              
+              <?php endif; ?>
                 
                 
                 </div>
