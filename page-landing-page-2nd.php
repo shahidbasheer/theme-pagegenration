3f3cdf9ab2b53da12e599f65e1c2a14c3ec7239b
Shahid Bashir <shahid.chodhry@gmail.com>
2016-01-24 19:45:58 +0500

    wordpressify

---
+++ page-landing-page-2nd.php
@@ -0,0 +1,103 @@
+<?php
+/*
+Template Name: landing-page-2nd
+*/
+?>
+
+<?php get_header ('landing-page'); ?>
+<link href='https://fonts.googleapis.com/css?family=Passion+One:400,700' rel='stylesheet' type='text/css'>
+
+ <link rel="stylesheet" href="<?php echo  theme_root . '/'; ?>css/style-landing-page.min.css">
+    <link rel="stylesheet" href="<?php echo  theme_root . '/'; ?>font-awesome/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Passion+One:400,700' rel='stylesheet' type='text/css'>
+    
+
+    <!-- Important Owl stylesheet -->
+    <link rel="stylesheet" href="<?php echo  theme_root . '/'; ?>css/owl.carousel.css">
+     
+    <!-- Default Theme -->
+    <link rel="stylesheet" href="<?php echo  theme_root . '/'; ?>css/owl.theme.css">
+
+
+            <!-- ****************************Nav End************************ -->
+                <div id="landing-page">
+                    
+                
+               <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                    
+                    <?php the_content(); ?>
+              
+
+              <?php endwhile;?>
+            
+                <?php else : ?>
+                <p> no content </p>
+              
+              <?php endif; ?>
+                
+                
+                </div>
+
+            <!-- **************************** insidetemplate ************************ -->
+ <?php get_footer('landing-page'); ?>
+
+ <script src="<?php echo  theme_root . '/'; ?>js/owl.carousel.min.js"></script>
+    
+    <script>
+           jQuery(document).ready(function($) {
+            var owl  = $('#demo');
+             $(owl).owlCarousel({
+            
+                 autoPlay: 3000, //Set AutoPlay to 3 seconds
+            
+                 items : 4,
+                 pagination: false,
+                 itemsDesktop : [1199,3],
+                 itemsDesktopSmall : [979,3]
+            
+             });
+
+
+
+             $('.indicator-next').click(function() {
+                console.log('clicked');
+                 owl.trigger('next.#dem');
+             })
+             // Go to the previous item
+             $('.indicator-pre').click(function() {
+                 // With optional speed parameter
+                 // Parameters has to be in square bracket '[]'
+                 owl.trigger('prev.#dem', [300]);
+             })
+            
+           });
+
+
+            jQuery(document).ready(function($) {
+            var owl  = $('#demo-2');
+             $(owl).owlCarousel({
+            
+                 autoPlay: 3000, //Set AutoPlay to 3 seconds
+            
+                 items : 4,
+                 pagination: false,
+                 itemsDesktop : [1199,3],
+                 itemsDesktopSmall : [979,3]
+            
+             });
+
+
+
+             $('.indicator-next').click(function() {
+                console.log('clicked');
+                 owl.trigger('next.#dem');
+             })
+             // Go to the previous item
+             $('.indicator-pre').click(function() {
+                 // With optional speed parameter
+                 // Parameters has to be in square bracket '[]'
+                 owl.trigger('prev.#dem', [300]);
+             })
+            
+           });
+    </script>
